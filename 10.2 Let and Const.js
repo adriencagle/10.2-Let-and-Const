cb832ee1a22667876333e0100e2f15cb3f8a7c7a
adriencagle <109845990+adriencagle@users.noreply.github.com>
2022-08-15 00:07:39 -0400

    Add files via upload

---
+++ 10.2 Let and Const.js
@@ -0,0 +1,7 @@
+const pi = 3.14;
+pi = 42;
+
+// Var and let have different use cases, and var is code scope vs let being block scope
+// Var is code scope and can be redefined, const is block scope and can't be redefined
+// let can be reassigned, const cannot be
+// Hoisting is running all the declarations of a variable before the rest of the code, which can cause unforeseen circumstances
